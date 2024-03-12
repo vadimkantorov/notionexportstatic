df71da71b2d4c813490a97cc11afef032b157c65
Vadim Kantorov <vadimkantorov@gmail.com>
2024-03-12 01:37:19 +0100

    ...

--- notionexportstatic.py
+++ notionexportstatic.py
@@ -343,7 +343,7 @@ def toggle2markdown(block, ctx, tag = '', markdown_icon = '', title_mode = False
 def headinglike2markdown(block, ctx, tag = ''):
     block_id_no_dashes = block['id'].replace('-', '')
     block_slug = get_heading_slug(block, ctx)
-    markdown_anchor = '\n<i id="{block_id_no_dashes}"></i><i id="{block_slug}"></i>\n' # f' [#](#{block_slug}) [#](#{block_id_no_dashes})'
+    markdown_anchor = f'\n<i id="{block_id_no_dashes}"></i><i id="{block_slug}"></i>\n' # f' [#](#{block_slug}) [#](#{block_id_no_dashes})'
     return (textlike2markdown if block.get(get_block_type(block), {}).get('is_toggleable') is not True else toggle2markdown)(block, ctx, tag = tag, markdown_icon = markdown_anchor)
 
 def linklike2markdown(block, ctx, tag = '', full_url_as_caption = True, line_break = False):
